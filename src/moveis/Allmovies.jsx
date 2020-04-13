2e0cff38d1ba108f9598688c1109c3210ce7bc31
unknown <yasser9946.1@gmail.com>
2020-04-13 14:14:35 +0300

    adde the Link

--- src/moveis/Allmovies.jsx
+++ src/moveis/Allmovies.jsx
@@ -1,29 +1,65 @@
 import React, { Component } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form } from 'react-bootstrap'
 import MoveiCard from './MoveiCard'
+import axios from "axios"
 
 export default class Allmovies extends Component {
+
+    state = {
+        allMoveis: [],
+        loading: false,
+        selectedMoveis: []
+    }
+
+    componentDidMount() {
+
+        axios.get('https://sei12.herokuapp.com/movei/json')
+            .then(res => {
+                this.setState({
+                    allMoveis: res.data
+                })
+            }).catch(err => console.log(err))
+
+
+
+    }
+
+
+
+
+
     render() {
+        console.log(this.state.allMoveis)
+
+        let allmoveis = this.state.allMoveis.map(movei => {
+
+
+            if (movei.link) return <MoveiCard movei={movei} />
+        })
+        let types = this.state.allMoveis.map(movei =>movei.typee)
+            types.unshift("All")
         return (
             <div>
                 <h1>All movie</h1>
                 <Container className="mt-5">
+                    <Row className="mt-5 justify-content-center">
+                        <Col md={4}>
+                        <Form.Label>Example select</Form.Label>
+                        <Form.Control as="select">
+                            {Array.from(new Set(types)).map(ele => (<option>{ele}</option>))}
+                          
+                        </Form.Control>
+                        </Col>
+                    </Row>
+                <Row className="mt-5 justify-content-center" >
 
-                <Row  className="mt-5 justify-content-center" >
-                
-                    <MoveiCard />
-                    <MoveiCard />
-                    <MoveiCard />
-                    <MoveiCard />
-                    <MoveiCard />
-                    <MoveiCard />
-                    <MoveiCard />
-                    <MoveiCard />
+                    {allmoveis}
+
                 </Row>
 
                 </Container>
 
-            </div>
+            </div >
         )
     }
 }
--- src/moveis/MoveiCard.jsx
+++ src/moveis/MoveiCard.jsx
@@ -3,14 +3,16 @@ import { Col, Image, Button } from 'react-bootstrap'
 
 export default class MoveiCard extends Component {
     render() {
+
+        let { YourName, link, name, typee, history } = this.props.movei
         return (
-            <Col md={3} className="m-2" style={{borderStyle :"double" , borderRadius : "30px", display:"flex" , flexDirection : "column" ,alignItems :"center"}}>
-                <Image roundedCircle style={{height:150 , width:150}} src="https://via.placeholder.com/150" ></Image>
-                <p>name</p>
-                <h5>history</h5>
-                <h5>type</h5>
-                <h4>Your name </h4>
-                <Button variant="outline-warning">more info</Button>
+            <Col md={3} className="m-2" style={{ borderStyle: "double", borderRadius: "30px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <Image roundedCircle style={{ height: 150, width: 150 }} src={link} ></Image>
+                <p>{this.props.movei.name}</p>
+                <h5>{history}</h5>
+                <h5>{typee}</h5>
+                <h4>{YourName} </h4>
+                <Button className="mb-3" variant="outline-warning">more info</Button>
 
             </Col>
         )
